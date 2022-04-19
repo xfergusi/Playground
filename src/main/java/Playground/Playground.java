e79856bad5cdd8e2d798951bfad647779b7f568a
Ian Ferguson <xfergusi@gmail.com>
2022-04-19 15:14:21 +1000

    just making sure we can compile and run

--- src/main/java/Playground/Playground.java
+++ src/main/java/Playground/Playground.java
@@ -1,7 +1,13 @@
 package Playground;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Playground {
     public static void main(String[] args) {
         System.out.println("hello from the playground");
+        HashMap<String, Integer> testerMap = new HashMap<>();
+        ArrayList<String> testerArrayList = new ArrayList<>();
+
     }
 }
